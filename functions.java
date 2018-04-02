35cc0bc6feffd3302fc6e264c36b1f4ad5563eec
bnweaver <bnweaver98@gmail.com>
2018-04-01 21:15:35 -0400

    First Commit

---
+++ functions.java
@@ -0,0 +1,142 @@
+package Networking;
+
+import java.util.Arrays;
+
+public class functions {
+	
+	public static String binary;
+	
+	public static int[] zeros;
+	public static int count1 = 0;
+	public static int count0 = 0;
+	public static int index = 0;
+	
+	public static boolean ready;
+
+	public static void runLength(String b) {
+		for (int x = 0; x < b.length(); x++) {
+			if ((b.charAt(x) == '0' || b.charAt(x) == '1') && b.charAt(b.length()-1) == '1') 
+				ready = true;
+			else 
+				ready = false;
+		}
+			
+		if (ready == true) {
+			for (int a = 0; a < b.length(); a++) {
+				if (b.charAt(a) == '1') {
+					count1++;
+				}
+			}
+			
+			zeros = new int[count1]; 
+			
+			for (int y = 0; y < b.length(); y++) {
+				if (b.charAt(y) == '0') {
+					count0++;
+				}
+				else if (b.charAt(y) == '1') {
+					if (count0 != 0) {
+						zeros[index] = count0;
+						count0 = 0;
+					}
+					else {
+						zeros[index] = 0;
+					}
+					index++;
+				}
+				
+			}
+			
+			networkGUI.printer.setText("Step One: Count the zeros\n"
+					+ Arrays.toString(zeros) + "\n\n" 
+					+ "Step Two: Converting zero counts into binary\n"
+					+ "Remember: 0-14 is 4 bits, 15-30 is 8 bits\n"
+					+ "Remember: All numbers above 15, start with 15's binary 4 bit of 1111 and end with the 4 bits that add to 15\n"
+					+ "Remember: Two 1s next to each other means there are zero 0s between them\n\n");
+			
+			String getText = networkGUI.printer.getText();
+			
+			for (int a = 0; a < zeros.length; a++) {
+				if (zeros[a] == 0) {
+					getText += "0 - 0000\n";
+				}
+				else if (zeros[a] < 8) {
+					if (Integer.toBinaryString(zeros[a]).length() < 2)
+						getText += zeros[a] + " - 000" + Integer.toBinaryString(zeros[a]) + "\n";
+					else if (Integer.toBinaryString(zeros[a]).length() < 3)
+						getText += zeros[a] + " - 00" + Integer.toBinaryString(zeros[a]) + "\n";
+					else if (Integer.toBinaryString(zeros[a]).length() < 4)
+						getText += zeros[a] + " - 0" + Integer.toBinaryString(zeros[a]) + "\n";
+				}
+				else if (zeros[a] == 15) {
+					getText += zeros[a] + Integer.toBinaryString(zeros[a]) + "\n";
+				}
+				else if (zeros[a] > 14) {
+					if (Integer.toBinaryString(zeros[a]-15).length() < 2)
+						getText += zeros[a] + " - 1111 000" + Integer.toBinaryString(zeros[a] -15) + "\n";
+					else if (Integer.toBinaryString(zeros[a]-15).length() < 3)
+						getText += zeros[a] + " - 1111 00" + Integer.toBinaryString(zeros[a] -15) + "\n";
+					else if (Integer.toBinaryString(zeros[a]-15).length() < 4)
+						getText += zeros[a] + " - 1111 0" + Integer.toBinaryString(zeros[a] -15) + "\n";
+					else
+						getText += zeros[a] + " - 1111 " + Integer.toBinaryString(zeros[a] - 15) + "\n"; 
+				}
+				else {
+					getText += zeros[a] + " - " + Integer.toBinaryString(zeros[a]) + "\n";
+				}
+			}
+			
+			getText += "\nStep Three: Combine\n";
+			
+			for (int y = 0; y < zeros.length; y++) {
+				if (zeros[y] == 0) {
+					getText += "0000 ";
+				}
+				else if (zeros[y] < 8) {
+					if (Integer.toBinaryString(zeros[y]).length() < 2)
+						getText += "000" + Integer.toBinaryString(zeros[y]) + " ";
+					else if (Integer.toBinaryString(zeros[y]).length() < 3)
+						getText += "00" + Integer.toBinaryString(zeros[y]) + " ";
+					else if (Integer.toBinaryString(zeros[y]).length() < 4)
+						getText += "0" + Integer.toBinaryString(zeros[y]) + " ";
+				}
+				else if (zeros[y] == 15) {
+					getText += Integer.toBinaryString(zeros[y]) + " ";
+				}
+				else if (zeros[y] > 14) {
+					if (Integer.toBinaryString(zeros[y]-15).length() < 2)
+						getText += "1111 000" + Integer.toBinaryString(zeros[y] -15) + " ";
+					else if (Integer.toBinaryString(zeros[y]-15).length() < 3)
+						getText += "1111 00" + Integer.toBinaryString(zeros[y] -15) + " ";
+					else if (Integer.toBinaryString(zeros[y]-15).length() < 4)
+						getText += "1111 0" + Integer.toBinaryString(zeros[y] -15) + " ";
+					else
+						getText += "1111 " + Integer.toBinaryString(zeros[y] - 15) + " "; 
+				}
+				else {
+					getText += Integer.toBinaryString(zeros[y]) + " ";
+				}
+			}
+			
+			networkGUI.printer.setText(getText);
+		}
+		else 
+			networkGUI.printer.setText("Error. Please check that you only submitted 1s and 0s and that your string ends in a 1. Hit Reset and Try Again");
+	}
+	
+	public void codeDivision() {
+		
+	}
+	
+	public void asynchronous() {
+		
+	}
+	
+	public void synchronous() {
+		
+	}
+	
+	public void crcError() {
+		
+	}
+}
---
+++ networkGUI.java
@@ -0,0 +1,148 @@
+package Networking;
+
+import java.awt.FlowLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.plaf.basic.BasicArrowButton;
+
+public class networkGUI extends JFrame implements ActionListener{
+
+	static JFrame gui = new JFrame("Networking Mathematics");
+	
+	JTextField textbox = new JTextField("Enter your code here");
+	JLabel instruction = new JLabel("Choose what you want to learn from the menu", SwingConstants.CENTER);
+	JButton enter = new JButton("Enter");
+	JButton reset = new JButton("Reset");
+	BasicArrowButton next = new BasicArrowButton(BasicArrowButton.EAST);
+	BasicArrowButton previous = new BasicArrowButton(BasicArrowButton.WEST);
+	static JTextArea printer = new JTextArea("Print");
+	JMenuBar bar = new JMenuBar();
+	JMenu menu = new JMenu("What do you want to learn?");
+	JMenuItem runLength = new JMenuItem("Lossless Compression: Run-Length Encoding");
+	JMenuItem codeDivision = new JMenuItem("Code Division Multiplexing");
+	JMenuItem asynchronous = new JMenuItem("Asynchronous Bit Calculation");
+	JMenuItem synchronous = new JMenuItem("Synchronous Bit Calculation");
+	JMenuItem crcError = new JMenuItem("Cyclic Redundancy Checksum");
+	
+	JPanel layout = new JPanel();
+	JPanel instructions = new JPanel(new FlowLayout());
+	JPanel button = new JPanel(new FlowLayout());
+	
+	int menuCode = 0;
+	
+	public networkGUI() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		setJMenuBar(bar);
+		bar.add(menu);
+		menu.add(runLength);
+		menu.add(codeDivision);
+		menu.add(asynchronous);
+		menu.add(synchronous);
+		menu.add(crcError);
+		
+		layout.setLayout(new BoxLayout(layout, BoxLayout.Y_AXIS));
+		add(layout);
+		
+		layout.add(instructions);
+		instructions.add(previous);
+		instructions.add(instruction);
+		instructions.add(next);
+		
+		layout.add(textbox);
+		layout.add(button);
+		button.add(reset);
+		button.add(enter);
+		layout.add(printer);
+		
+		printer.setMargin(new Insets(20, 20, 0, 20));
+		printer.setEditable(false);
+		
+		
+		setSize(750,500);
+		setVisible(true);
+		
+		runLength.addActionListener(this);
+		codeDivision.addActionListener(this);
+		asynchronous.addActionListener(this);
+		synchronous.addActionListener(this);
+		crcError.addActionListener(this);
+		enter.addActionListener(this);
+		reset.addActionListener(this);
+	}
+	
+	public void actionPerformed(ActionEvent a) {
+		Object source = a.getSource();
+		
+		if (source == reset) {
+			printer.setText("");
+			textbox.setText("");
+			functions.binary = "";
+			functions.count0 = 0;
+			functions.count1 = 0;
+			functions.index = 0;
+			functions.zeros = new int[0];
+		}
+		
+		if (source == enter) {
+			if (menuCode == 1) {
+				functions.binary = textbox.getText();
+				functions.runLength(functions.binary);
+			}
+			if (menuCode == 2) {
+				
+			}
+			if (menuCode == 3) {
+				
+			}
+			if (menuCode == 4) {
+				
+			}
+			if (menuCode == 5) {
+				
+			}
+		}
+		
+		if (source == runLength) {
+			menuCode = 1;
+			instruction.setText("Enter a string of 0s and 1s and then press Enter  (Make sure to end with a 1!)");
+		}
+		
+		if (source == codeDivision) {
+			menuCode = 2;
+			
+		}
+		
+		if (source == asynchronous) {
+			menuCode = 3;
+			
+		}
+		
+		if (source == synchronous) {
+			menuCode = 4;
+			
+		}
+		
+		if (source == crcError) {
+			menuCode = 5;
+		}
+	}
+	
+	public static void main(String[] args) {
+		gui = new networkGUI();
+	}
+}
